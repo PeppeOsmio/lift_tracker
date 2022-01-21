3fdf040f40ba8eb611a2d3fcacb6d187afdc9225
PeppeOsmio <bosius00@gmail.com>
2022-01-21 01:38:43 +0100

    prova

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lift_tracker/app.dart';
 
-//modifica di prova
-//prova provaaaa
-
 void main() {
   runApp(const MaterialApp(home: App()));
 }
