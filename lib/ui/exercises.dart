d2b63f599d65c4b65f976c80dbc38d64eb5c2154
PeppeOsmio <bosius00@gmail.com>
2022-04-21 01:53:18 +0200

    commit

--- lib/ui/exercises.dart
+++ lib/ui/exercises.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lift_tracker/data/classes/exercisedata.dart';
+import 'package:lift_tracker/data/helper.dart';
 import 'package:lift_tracker/gym_icons_icons.dart';
 import 'package:lift_tracker/ui/widgets.dart';
 import 'colors.dart';
@@ -23,10 +24,48 @@ class _ExercisesState extends State<Exercises> {
         children: [
           SearchBar(hint: 'Filter...', textController: TextEditingController()),
           Expanded(
-            child: ListView(
-              children: [],
-            ),
-          ),
+              child: FutureBuilder(
+                  future: Helper.getExerciseData(),
+                  builder: (context, ss) {
+                    if (ss.hasData) {
+                      List<ExerciseData> exerciseData =
+                          ss.data! as List<ExerciseData>;
+                      return Padding(
+                        padding: const EdgeInsets.only(top: 16),
+                        child: ListView.separated(
+                            separatorBuilder: (context, index) {
+                              return SizedBox(
+                                height: 16,
+                              );
+                            },
+                            itemCount: exerciseData.length,
+                            itemBuilder: (context, index) {
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.pop(context, exerciseData[index]);
+                                },
+                                child: Padding(
+                                  padding: EdgeInsets.only(
+                                      left: 16,
+                                      right: 16,
+                                      bottom: index == exerciseData.length - 1
+                                          ? 16
+                                          : 0),
+                                  child: ExerciseCard(
+                                      exerciseData: exerciseData[index]),
+                                ),
+                              );
+                            }),
+                      );
+                    }
+                    if (ss.hasError) {
+                      return Expanded(
+                          child: Center(
+                        child: Text('Error'),
+                      ));
+                    }
+                    return SizedBox();
+                  })),
         ],
       ),
     );
